fa6d49c367c61e5ad0798eabc36ad3b83c52d987
Aria <aria@Arias-MacBook-Pro.local>
2014-07-20 00:58:45 -0700

    added buncha files

--- backend/careerbrowser/urls.py
+++ backend/careerbrowser/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'careerbrowser.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'careerbrowser_core.views.home'),#careerbrowser_core.views.home'),
+    url(r'^$', 'careerbrowser_core.views.home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', 'careerbrowser_core.views.api_handler')
 )
