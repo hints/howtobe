94fea9b34bcc6d7ca9ba9d4c71363546d724a2b6
Aria <aria@Arias-MacBook-Pro.local>
2014-07-20 01:46:31 -0700

    updated views API method

--- backend/careerbrowser/careerbrowser_core/views.py
+++ backend/careerbrowser/careerbrowser_core/views.py
@@ -3,34 +3,40 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+import json
 
 # Create your views here.
 
 def home(request):
 	if settings.GLOVAR:
 		settings.GLOCOUNT += 1
-		return HttpResponse(settings.GLOVAR + str(settings.GLOCOUNT))#"global!")
+		return HttpResponse(settings.GLOVAR + str(settings.GLOCOUNT))
 	else:
 		return HttpResponse("no global")
 
-#	return HttpResponse("Hello!")
-
 #exempting from safeguard against XSS attacks
 #DONT DO THIS
-@csrf_exempt
-def api_handler(request):
-	if request.method == 'GET':
-		print("API CALL MADE")
-		return HttpResponse("Use POST to request API data pl0x")
-	elif request.method == 'POST':
-		return HttpResponse("API CALL MADE!\n")#, context_instance=RequestContext(request))
-	#http response as POST
-
+# @csrf_exempt
 # def api_handler(request):
 # 	if request.method == 'GET':
 # 		print("API CALL MADE")
 # 		return HttpResponse("Use POST to request API data pl0x")
 # 	elif request.method == 'POST':
-# 		c = RequestContext(c, {'key':'value'})
-# 		#return HttpResponse(c)
-# 		return render_to_response("teaset",c)
+# 		return HttpResponse("API CALL MADE!\n")#, context_instance=RequestContext(request))
+# 	#http response as POST
+
+@csrf_exempt
+def api_handler(request, profession):
+	print(profession)
+	if request.method == 'GET':
+		print("API CALL MADE")
+		return HttpResponse("Use POST to request API data please")
+	elif request.method == 'POST':
+		nodes = settings.R_DATA["nodes"].get(profession, None)
+		edges = settings.R_DATA["edges"].get(profession, None)
+		res = { "edges": edges, "nodes": nodes}
+		if(res.get("edges") is None or res.get("nodes is None")):
+			return(json.dumps({}))
+
+		#return HttpResponse("API CALL MADE!\n")#, context_instance=RequestContext(request))
+		return HttpResponse(json.dumps(res))
